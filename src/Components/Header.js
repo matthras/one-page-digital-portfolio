3619a26d7a1d6bc01ec70c6a0388bf738736b2b7
matthras <matthew.y.p.mack@gmail.com>
2020-02-08 12:16:25 +1100

    swapped public & src files with the one from that repo

---
+++ src/Components/Header.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var occupation= this.props.data.occupation;
+      var description= this.props.data.description;
+      var networks= this.props.data.social.map(function(network){
+        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      })
+    }
+
+    let config = {
+      num: [4, 7], // number of shapes to generate per tick
+      rps: 1, // ticks per second
+      radius: 40, // radius from middle, write as single number if you want constant, array as range e.g. [5,40]
+      life: 3, // life in seconds before it disappears? single number for constant, array for range e.g. [1.5,3]
+      v: [2,5], // how far vertically to throw the shapes upon initial spawn, single number for constant, array for range
+      tha: [-40, 40],
+      // body: "/images/profilepic.jpg", // Whether to render pictures
+      // rotate: [0, 20],
+      alpha: [0.6, 0],
+      scale: [1, 0.1],
+      position: "all", // all or center or {x:1,y:1,width:100,height:100}
+      color: ["random", "#ff0000"],
+      cross: "dead", // cross or bround
+      random: 15,  // or null,
+      g: 5,    // gravity
+      // f: [2, -1], // force
+      onParticleUpdate: (ctx, particle) => {
+          ctx.beginPath();
+          ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
+          ctx.fillStyle = particle.color;
+          ctx.fill();
+          ctx.closePath();
+      }
+    };
+
+    return (
+      <header id="home">
+      <nav id="nav-wrap">
+
+         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+         <ul id="nav" className="nav">
+            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+            <li><a className="smoothscroll" href="#about">About</a></li>
+	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
+            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+            <li><a className="smoothscroll" href="#contact">Contact</a></li>
+         </ul>
+
+      </nav>
+
+      <div className="row banner">
+         <div className="banner-text">
+            <h1 className="responsive-headline">{name}</h1>
+            <h2><span>{occupation}</span></h2>
+            <h3><span>{description}</span></h3>
+            <hr />
+            <ul className="social">
+               {networks}
+            </ul>
+         </div>
+      </div>
+
+      <p className="scrolldown">
+         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+      </p>
+      
+   </header>
+    );
+  }
+}
+
+export default Header;
